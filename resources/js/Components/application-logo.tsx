35c8a2706101e41809206ab9bab9d10250c68761
Ervalsa Dwi Nanda <ervalsananda@gmail.com>
2026-01-24 10:17:53 +0700

    fix: foldering

--- resources/js/Components/application-logo.tsx
+++
@@ -1,5 +0,0 @@
-export default function ApplicationLogo(props: React.ImgHTMLAttributes<HTMLImageElement>) {
-    return (
-        <img src="/assets/selia.webp" alt="Selia Logo" {...props} />
-    );
-}
