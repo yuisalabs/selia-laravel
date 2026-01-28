cdf60f537e104518f8d4c747a7f5ee0098aa3b77
Ervalsa Dwi Nanda <ervalsananda@gmail.com>
2026-01-28 08:35:15 +0700

    fix: responsive guest layout

--- resources/js/layouts/guest-layout.tsx
+++ resources/js/layouts/guest-layout.tsx
@@ -6,12 +6,12 @@ import { PropsWithChildren } from 'react';
 
 export default function GuestLayout({ children }: PropsWithChildren) {
     return (
-        <div className="flex min-h-screen flex-col items-center bg-background pt-6 sm:justify-center sm:pt-0">
+        <div className="flex min-h-screen flex-col items-center justify-center bg-background px-4 sm:pt-0">
             <div className="absolute top-4 right-4">
                 <ThemeToggle />
             </div>
             
-            <Card className="mt-6 w-full overflow-hidden bg-card px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
+            <Card className="mx-4 mt-6 w-full overflow-hidden bg-card px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
                 <Link href="/" className="flex justify-center">
                     <ApplicationLogo className="h-10 w-10 fill-current text-muted" />
                 </Link>
